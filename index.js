b6e64de63a5e18fed38600e936a310335d0f8325
iKameo <the.ikameo@gmail.com>
2021-03-23 14:00:57 +0100

    added a basic alert to get show the value from a input

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const http = require('http');
-
 const fs = require('fs');
 const _hostname = '127.0.0.1';
 const _port = 3000;
-
 
 //basic webserver
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-type', 'text/html');
-    var _html = fs.readFileSync('./index.html');
+    var _html = fs.readFileSync('./public/index.html');
     res.write(_html);
     res.end();
 });
@@ -17,4 +15,3 @@ const server = http.createServer((req, res) => {
 server.listen(_port, _hostname, () => {
     console.log(`🌏Server running at http://${_hostname}:${_port}/`);
 });
-
