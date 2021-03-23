f45ddf984cd609085acaf8e27ed3901c8ed58b43
iKameo <the.ikameo@gmail.com>
2021-03-23 12:29:28 +0100

    barebones node.js which loads a html site

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const http = require('http');
 
+const fs = require('fs');
 const _hostname = '127.0.0.1';
 const _port = 3000;
 
 
 //basic webserver
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    })
-    res.write("Hello World");
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/html');
+    var _html = fs.readFileSync('./index.html');
+    res.write(_html);
     res.end();
 });
 
